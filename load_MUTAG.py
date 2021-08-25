36ae0cea16219d3557a7b838c4c9426d36310e85
NickDrake117 <63723473+NickDrake117@users.noreply.github.com>
2021-08-25 09:05:23 +0200

    Add files via upload

---
+++ load_MUTAG.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from GNN.composite_graph_class import CompositeGraphObject
+from GNN.graph_class import GraphObject
+
+# load numpy from txt
+path = 'MUTAG_raw/'
+edgesIDs = np.loadtxt(path + 'Mutagenicity_edges.txt', dtype=int, delimiter=', ')
+edgesL = np.loadtxt(path + 'Mutagenicity_edge_labels.txt', dtype=int)
+nodesL = np.loadtxt(path + 'Mutagenicity_node_labels.txt', dtype=int)
+gIDs_nodes = np.loadtxt(path + 'Mutagenicity_graph_indicator.txt', dtype=int)
+gtargs = np.loadtxt(path + 'Mutagenicity_graph_labels.txt', dtype=int)
+
+# retrieve useful indices
+_, idx = np.unique(gIDs_nodes, return_index=True)
+idx = np.concatenate([idx, [len(gIDs_nodes)]])
+idx = idx.tolist()
+
+# NODES
+print('Nodes', end='\t')
+# encode labels in 1-hot vectors
+nL = np.zeros((nodesL.shape[0], len(np.unique(nodesL))), dtype=int)
+nL[range(nL.shape[0]), nodesL] = 1
+nodes = [nL[i:j, :] for i, j in zip(idx[:-1], idx[1:])]
+print('OK')
+
+# EDGES
+print('Edges', end='\t')
+edgesIDs = np.unique(edgesIDs, axis=0)
+# check edge membership in each graph
+eids = [k[:, 0] * k[:, 1] for k in [(edgesIDs > i) * (edgesIDs <= j) for i, j in zip(idx[:-1], idx[1:])]]
+eIDs = [edgesIDs[i, :] for i in eids]
+# rename nodes indices
+for i in eIDs:
+    unique = np.unique(i)
+    new_vals = range(len(unique))
+    for k, elem in enumerate(unique): i[i == elem] = new_vals[k]
+# encode labels in 1-hot vectors
+eL = np.zeros((edgesL.shape[0], len(np.unique(edgesL))), dtype=int)
+eL[range(eL.shape[0]), edgesL] = 1
+# concatenate [id1, id2, label]
+edges = [np.concatenate([eIDs[i], eL[eids[i]]], axis=1) for i in range(len(eIDs))]
+print('OK')
+
+# TARGETS
+print('Targets', end='\t')
+# encode labels in 1-hot vectors
+targs = np.zeros((len(gtargs), len(np.unique(gtargs))), dtype=int)
+targs[range(len(targs)), gtargs] = 1
+print('OK')
+
+# CREATE GRAPHS
+graphs = [GraphObject(arcs=e, nodes=n, targets=t[np.newaxis, ...], problem_based='g') for e, n, t in zip(edges, nodes, targs)]
+
+# COMPOSITE GRAPHS
+composite_graphs = [CompositeGraphObject(arcs=g.arcs, nodes=g.nodes, targets=g.targets, problem_based='g',
+                                         type_mask=np.ones((g.nodes.shape[0], 1), dtype=bool), dim_node_labels=(g.nodes.shape[1],))
+                    for g in graphs]
---
+++ starter.py
@@ -0,0 +1,196 @@
+import os
+import shutil
+from typing import Optional, Union
+
+import tensorflow as tf
+from numpy import random
+
+from GNN import GNN_utils as utils
+from GNN.GNNkeras import GNNnodeBased, GNNedgeBased, GNNgraphBased
+from GNN.GraphGenerator import GraphDataGenerator
+from GNN.LGNNkeras import LGNN
+from GNN.MLP import MLP, get_inout_dims
+from GNN.graph_class import GraphObject
+
+#######################################################################################################################
+# SCRIPT OPTIONS - modify the parameters to adapt the execution to the problem under consideration ####################
+#######################################################################################################################
+
+# MUTAG option - if True, gnn/lgnn is trained on a real-world dataset MUTAG
+# problem is set automatically to graph classification -> addressed_problem='c', problem_based='g'
+use_MUTAG: bool = False
+
+# GENERIC GRAPH PARAMETERS. See utils.randomGraph for details
+# Node and edge labels are initialized randomly. Target clusters are given by sklearn.
+# Each graph has at least <min_nodes_number> nodes and at most <max_nodes_number> nodes
+# Possible <aggregation_mode> for matrix ArcNoe belonging to graphs in ['average', 'normalized', 'sum']
+# problem_based in ['n', 'a','g'] -> ['c' classification, 'r' regression]
+# addressed_problem in ['c', 'r'] -> ['g' graph-based; 'n' node-based; 'a' arc-based;]
+problem_based       : str = 'n'
+addressed_problem   : str = 'c'
+graphs_number       : int = 1000
+min_nodes_number    : int = 15
+max_nodes_number    : int = 40
+dim_node_label      : int = 5
+dim_arc_label       : int = 1
+dim_target          : int = 2
+density             : float = 0.7
+aggregation_mode    : str = 'average'
+
+# LEARNING SETS PARAMETERS
+perc_Train      : float = 0.7
+perc_Valid      : float = 0.2
+batch_size      : int = 32
+normalize       : bool = True
+seed            : Optional[int] = None
+norm_nodes_range: Optional[tuple[Union[int, float], Union[int, float]]] = None  # (-1,1) # other possible value
+norm_arcs_range : Optional[tuple[Union[int, float], Union[int, float]]] = None  # (0,1) # other possible value
+
+# NET STATE PARAMETERS
+activations_net_state   : str = 'selu'
+kernel_init_net_state   : str = 'lecun_normal'
+bias_init_net_state     : str = 'lecun_normal'
+kernel_reg_net_state    : str = None
+bias_reg_net_state      : str = None
+dropout_rate_st         : float = 0.1
+dropout_pos_st          : Union[list[int], int] = 0
+hidden_units_net_state  : Optional[Union[list[int], int]] = None
+
+### NET OUTPUT PARAMETERS
+activations_net_output  : str = 'softmax'
+kernel_init_net_output  : str = 'glorot_normal'
+bias_init_net_output    : str = 'glorot_normal'
+kernel_reg_net_output   : str = None
+bias_reg_net_output     : str = None
+dropout_rate_out        : float = 0.1
+dropout_pos_out         : Union[list[int], int] = 0
+hidden_units_net_output : Optional[Union[list[int], int]] = None
+
+# GNN PARAMETERS
+dim_state       : int = 10
+max_iter        : int = 5
+state_threshold : float = 0.01
+
+# LGNN PARAMETERS
+layers          : int = 3
+get_state       : bool = False
+get_output      : bool = True
+training_mode   : str = 'parallel'
+
+# LOSS / OPTIMIZER PARAMETERS
+loss_function   : tf.keras.losses = tf.keras.losses.categorical_crossentropy
+optimizer       : tf.keras.optimizers = tf.optimizers.Adam(learning_rate=0.001)
+
+# TRAINING PARAMETERS
+# callbacks
+path_writer : str = 'writer/'
+monitored   : str = 'val_loss'
+patience    : int = 10
+# training procedure
+epochs      : int = 10
+
+#######################################################################################################################
+# SCRIPT ##############################################################################################################
+#######################################################################################################################
+
+### LOAD DATASET
+if use_MUTAG:
+    # from MUTAG
+    addressed_problem = 'c'
+    problem_based = 'g'
+    from load_MUTAG import graphs
+
+else:
+    # random graphs
+    graphs = [utils.randomGraph(nodes_number=int(random.choice(range(min_nodes_number, max_nodes_number))),
+                                dim_node_label=dim_node_label,
+                                dim_arc_label=dim_arc_label,
+                                dim_target=dim_target,
+                                density=density,
+                                normalize_features=False,
+                                aggregation_mode=aggregation_mode,
+                                problem_based=problem_based)
+              for i in range(graphs_number)]
+
+### PREPROCESSING
+# SPLITTING DATASET in Train, Validation and Test set
+iTr, iTe, iVa = utils.getindices(len(graphs), perc_Train, perc_Valid, seed=seed)
+gTr = [graphs[i] for i in iTr]
+gTe = [graphs[i] for i in iTe]
+gVa = [graphs[i] for i in iVa]
+gGen = gTr[0].copy()
+
+# GRAPHS NORMALIZATION, based on training graphs
+if normalize:
+    utils.normalize_graphs(gTr, gVa, gTe,
+                           based_on='gTr',
+                           norm_rangeN=norm_nodes_range,
+                           norm_rangeA=norm_arcs_range)
+
+### MODELS
+# MLP NETS - STATE
+input_net_st, layers_net_st = zip(*[get_inout_dims(net_name='state', dim_node_label=gGen.DIM_NODE_LABEL,
+                                                   dim_arc_label=gGen.DIM_ARC_LABEL, dim_target=gGen.DIM_TARGET,
+                                                   problem_based=problem_based, dim_state=dim_state,
+                                                   hidden_units=hidden_units_net_state,
+                                                   layer=i, get_state=get_state, get_output=get_output) for i in range(layers)])
+nets_St = [MLP(input_dim=i, layers=j,
+               activations=activations_net_state,
+               kernel_initializer=kernel_init_net_state,
+               bias_initializer=bias_init_net_state,
+               kernel_regularizer=kernel_reg_net_state,
+               bias_regularizer=bias_reg_net_state,
+               dropout_rate=dropout_rate_st,
+               dropout_pos=dropout_pos_st,
+               name=f'State_{idx}') for idx, (i, j) in enumerate(zip(input_net_st, layers_net_st))]
+
+# MLP NETS - OUTPUT
+input_net_out, layers_net_out = zip(*[get_inout_dims(net_name='output', dim_node_label=gGen.DIM_NODE_LABEL,
+                                                     dim_arc_label=gGen.DIM_ARC_LABEL, dim_target=gGen.DIM_TARGET,
+                                                     problem_based=problem_based, dim_state=dim_state,
+                                                     hidden_units=hidden_units_net_output,
+                                                     layer=i, get_state=get_state, get_output=get_output) for i in range(layers)])
+nets_Out = [MLP(input_dim=i, layers=j,
+                activations=activations_net_output,
+                kernel_initializer=kernel_init_net_output,
+                bias_initializer=bias_init_net_output,
+                kernel_regularizer=kernel_reg_net_output,
+                bias_regularizer=bias_reg_net_output,
+                dropout_rate=dropout_rate_out,
+                dropout_pos=dropout_pos_out,
+                name=f'Out_{idx}') for idx, (i, j) in enumerate(zip(input_net_out, layers_net_out))]
+
+# template model
+model = {'n': GNNnodeBased, 'a': GNNedgeBased, 'g': GNNgraphBased}[problem_based]
+
+# GNN
+gnn = model(nets_St[0], nets_Out[0], dim_state, max_iter, state_threshold).copy()
+gnn.compile(optimizer=optimizer, loss=loss_function, average_st_grads=False, metrics=['accuracy', 'mse'], run_eagerly=True)
+
+# LGNN
+lgnn = LGNN([model(s, o, dim_state, max_iter, state_threshold) for s, o in zip(nets_St, nets_Out)], get_state, get_output)
+lgnn.compile(optimizer=optimizer, loss=loss_function, average_st_grads=True, metrics=['accuracy', 'mse'], run_eagerly=True,
+             training_mode=training_mode)
+
+### DATA PROCESSING
+# data generator
+gTr_Generator = GraphDataGenerator(gTr, problem_based, aggregation_mode)
+gVa_Generator = GraphDataGenerator(gVa, problem_based, aggregation_mode)
+gTe_Generator = GraphDataGenerator(gTe, problem_based, aggregation_mode)
+
+### TRAINING PROCEDURE
+if os.path.exists(path_writer): shutil.rmtree(path_writer)
+
+# callbacks for single gnn
+tensorboard_gnn = tf.keras.callbacks.TensorBoard(log_dir=f'{path_writer}single_gnn/', histogram_freq=1)
+early_stopping_gnn = tf.keras.callbacks.EarlyStopping(monitor=monitored, restore_best_weights=True, patience=patience)
+callbacks_gnn = [tensorboard_gnn, early_stopping_gnn]
+
+# callbacks for lgnn
+tensorboard_lgnn = [tf.keras.callbacks.TensorBoard(log_dir=f'{path_writer}gnn{i}/', write_graph=False , histogram_freq=1) for i in range(lgnn.LAYERS)]
+early_stopping_lgnn = [tf.keras.callbacks.EarlyStopping(monitor=monitored, restore_best_weights=True, patience=patience) for i in range(lgnn.LAYERS)]
+callbacks_lgnn = list(zip(tensorboard_lgnn, early_stopping_lgnn))
+if training_mode != 'serial': callbacks_lgnn = callbacks_lgnn[0]
+
+# gnn.fit(gTr_Generator, epochs=epochs, validation_data=gVa_Generator, callbacks=callbacks_gnn)
+# lgnn.fit(gTr_Generator, epochs=epochs, validation_data=gVa_Generator, callbacks=callbacks_lgnn)
